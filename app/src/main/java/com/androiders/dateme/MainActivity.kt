055623f65a64eac0d846319330d26bc5b43c6fd7
borkusgod <lightraven.bourque@gmail.com>
2022-11-17 07:08:36 -0400

    Getting everything set up and in place so I can start plugging away at components.

--- app/src/main/java/com/androiders/dateme/MainActivity.kt
+++ app/src/main/java/com/androiders/dateme/MainActivity.kt
@@ -3,18 +3,15 @@ package com.androiders.dateme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -35,50 +32,64 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text(
-                        buildAnnotatedString {
-                            withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
-                                withStyle(style = SpanStyle(color = MaterialTheme.colors.primary)) {
-                                    append("Hello\n")
-                                }
-                                withStyle(
-                                    style = SpanStyle(
-                                        fontWeight = FontWeight.Bold,
-                                        color = MaterialTheme.colors.primary
-                                    )
-                                ) {
-                                    append("DatingMee\n")
-                                }
-                                append("App made")
-                            }
-                        }
-                    )
-
+                    OldStephaneFunc()
                 }
             }
         }
     }
 }
 
+@Composable
+fun OldStephaneFunc() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(250.dp)
+                .padding(20.dp),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 55.sp,
+                                color = MaterialTheme.colors.primary)
+
+                        ) {
+                            append("Hello\n")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 25.sp,
+                                color = MaterialTheme.colors.primary
+                            )
+                        ) {
+                            append("DatingMee\n")
+                        }
+                        append("App made")
+                    }
+                }
+            )
+        }
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DateMeTheme {
-        Greeting("Android")
-    }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun Greeting(name: String = "stephane") {
-//    Text(text = "Helllo $name", fontSize = 22.sp, fontWeight = FontWeight.Bold)
-    Spacer(modifier = Modifier.height(8.dp))
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-
+        OldStephaneFunc()
     }
 }
