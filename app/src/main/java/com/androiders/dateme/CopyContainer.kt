3e737b046e69aea727a5e27958b18b5b3d10f936
borkusgod <lightraven.bourque@gmail.com>
2022-11-17 11:20:38 -0400

    Just putting all the pieces in place. Been trying to get dependency errors under control for the onboarding part.

---
+++ app/src/main/java/com/androiders/dateme/CopyContainer.kt
@@ -0,0 +1,30 @@
+package com.androiders.dateme
+
+/*
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 55.sp,
+                                color = MaterialTheme.colors.primary)
+
+                        ) {
+                            append("Hello\n")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 25.sp,
+                                color = MaterialTheme.colors.primary
+                            )
+                        ) {
+                            append("DatingMee\n")
+                        }
+                        append("App made")
+                    }
+                }
+            )
+
+ */
--- app/src/main/java/com/androiders/dateme/MainActivity.kt
+++ app/src/main/java/com/androiders/dateme/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,42 +43,99 @@ fun OldStephaneFunc() {
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.LightGray),
-        contentAlignment = Alignment.Center
+        //contentAlignment = Alignment.Center
     ) {
         Column(
             modifier = Modifier
                 .fillMaxHeight()
-                .width(250.dp)
-                .padding(20.dp),
-            verticalArrangement = Arrangement.SpaceAround,
+                .width(440.dp)
+                .padding(35.dp),
+            verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                buildAnnotatedString {
-                    withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
-                        withStyle(
-                            style = SpanStyle(
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 55.sp,
-                                color = MaterialTheme.colors.primary)
+            Row() {
 
-                        ) {
-                            append("Hello\n")
-                        }
-                        withStyle(
-                            style = SpanStyle(
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 25.sp,
-                                color = MaterialTheme.colors.primary
-                            )
-                        ) {
-                            append("DatingMee\n")
-                        }
-                        append("App made")
-                    }
+            }
+            Button(
+                modifier = Modifier
+                    .width(380.dp)
+                ,
+                onClick = {},
+                shape = RoundedCornerShape(50),
+//                contentPadding = PaddingValues(
+//                    start = 0.dp,
+//                    top = 12.dp,
+//                    end = 20.dp,
+//                    bottom = 12.dp,
+//                )
+                ) {
+                // Inner content including an icon and a text label
+                Box(modifier = Modifier.padding(5.dp),
+                    ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.logo_z_tran_white),
+                        contentDescription = "Favorite",
+                        modifier = Modifier
+                            .size(45.dp)
+                    )
+                    Text(text = "Log in with Username or Email",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.Center),
+                        textAlign = TextAlign.End
+
+                    )
+
                 }
+            }
+
+            Spacer(modifier = Modifier
+                .size(20.dp)
             )
-        }
+
+            Button(
+                modifier = Modifier
+                    .width(380.dp)
+                ,
+                onClick = {},
+                shape = RoundedCornerShape(50),
+//                contentPadding = PaddingValues(
+//                    start = 0.dp,
+//                    top = 12.dp,
+//                    end = 20.dp,
+//                    bottom = 12.dp,
+//                )
+            ) {
+                // Inner content including an icon and a text label
+                Box(modifier = Modifier.padding(5.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.logo_z_tran_white),
+                        contentDescription = "Favorite",
+                        modifier = Modifier
+                            .size(45.dp)
+                    )
+                    Text(text = "Log in with Username or Email",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.Center),
+                        textAlign = TextAlign.End
+
+                    )
+
+                }
+            }
+            Text(
+                text = "Having trouble logging in?",
+                fontFamily = FontFamily.Serif,
+                fontSize = 14.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .clickable {}
+                    .padding(25.dp),
+                textAlign = TextAlign.Center
+            )
+        } // Start of page column
     }
 }
 
